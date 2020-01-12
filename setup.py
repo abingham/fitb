70ec5686b511e3622625501207a9a19379a58168
Austin Bingham <austin.bingham@gmail.com>
2020-01-12 20:58:28 +0000

    Bump version: 1.1.2 → 2.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fitb',
-    version='1.1.2',
+    version='2.0.0',
     packages=find_packages('src'),
 
     author='Austin Bingham',
